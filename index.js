adba66fed347410f201ecc8d8af747bb005c59ea
Ali-10011 <l201154@lhr.nu.edu.pk>
2024-01-09 23:12:53 +0500

    feat: add supabase, add send and receive msg

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const {createClient} = require('@supabase/supabase-js')
 const bodyparser = require('body-parser')
 const {config} = require('dotenv')
 const cors = require('cors')
 config()
 const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_ACCOUNT_AUTH_TOKEN);
-
+//supabase client
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
 
 
@@ -16,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.listen(process.env.PORT,()=>{
-    console.log("App is listening on port 3001")
+    console.log(`App is listening on port ${process.env.PORT}`)
 })
 
 app.post('/send-new-sms', async (req,res)=>{
@@ -36,8 +38,34 @@ app.post('/send-new-sms', async (req,res)=>{
         from: 'whatsapp:+14155238886',
         to: 'whatsapp:' + user.wirelessPrimaryPhoneNum1
     })
-    .then(message => console.log(message.sid))
-    
+    .then( async (message) => {console.log(message.sid)})
+
+      const {error} = await supabase
+      .from('ChatContext')
+      .insert({
+          PhoneNumber: user.wirelessPrimaryPhoneNum1,
+          Conversations: [{
+            msg: message,
+            timestamp: Date.now(),
+            user: "System"
+          }],
+          Summary: "Testing",
+      });
+
+      const test = await supabase
+      .from('Customer')
+      .insert({
+          PhoneNumber: user.wirelessPrimaryPhoneNum1,
+          Name: user.ownerName,
+          Address: user.address,
+          Price: user.price,
+          Bedrooms: user.bedrooms,
+          Bathrooms: user.bathrooms,
+          ApproxSquare: user.approxSquare,
+          DaysOnMarket: user.daysOnMarket     
+      });
+ 
+      console.log(test)
     }
     else {
 
@@ -48,14 +76,70 @@ app.post('/send-new-sms', async (req,res)=>{
         to: 'whatsapp:' + user.wirelessPrimaryPhoneNum2
     })
     .then(message => console.log(message.sid))
+
+
+    const {error} = await supabase
+    .from('ChatContext')
+    .insert({
+        PhoneNumber: user.wirelessPrimaryPhoneNum2,
+        Conversations: [{
+          msg: message,
+          timestamp: Date.now(),
+          user: "System"
+        }],
+        Summary: "Testing",
+    });
+    
+    const test = await supabase
+    .from('Customer')
+    .insert({
+        PhoneNumber: user.wirelessPrimaryPhoneNum2,
+        Name: user.ownerName,
+        Address: user.address,
+        Price: user.price,
+        Bedrooms: user.bedrooms,
+        Bathrooms: user.bathrooms,
+        ApproxSquare: user.approxSquare,
+        DaysOnMarket: user.daysOnMarket     
+    });
+
+  console.log(test)
   }
-
-  
-
   }));
  
  
-  return res.status(200).json({text:"I got your request"})
+  return res.status(200).json({text: "I got your request"})
+
+})
+
+
+
+app.post('/receive-msg', async (req,res)=>{
+
+
+client.messages
+.create({
+    body: "g",
+    from: 'whatsapp:+14155238886',
+    to: req.body.From
+})
+
+const {data, error} = await supabase
+        .from('ChatContext')
+        .select()
+        .eq('PhoneNumber', `${req.body.From.split(":")[1]}`)
+
+data[0].Conversations.push({msg: req.body.Body, timestamp: Date.now(), user: "Client"})
+data[0].Conversations.push({msg: "g", timestamp: Date.now(), user: "System"})
+
+const { updateError } = await supabase
+        .from('ChatContext')
+        .update({ Conversations: data[0].Conversations})
+        .eq('PhoneNumber', `${req.body.From.split(":")[1]}`)
+
+
+
+ return res.status(200).json({msg: 'Successfully Replied'});
 
 })
 
